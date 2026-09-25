cfcc9b0bb899c987307183fd8c3f80de7c8bec4e
rushikesh-void <rushibalwant58@gmail.com>
2026-09-25 12:39:41 +0530

    Fix JWT_SECRET casing in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
 const generateToken= (userId)=>{
-    return jwt.sign({id: userId}, process.env.Jwt_secret, {
+    return jwt.sign({id: userId}, process.env.JWT_SECRET, {
         expiresIn: '7d'
     })
 }
